721a94216c8860bebcf713b2514ba13e671b7761
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-03-07 10:17:24 +0100

    feat: Organize project structure and add Navbar components. - Create 'Auth' and 'Base' folders in the 'pages' directory. - In the 'Navbar' folder, add components: - NavBar.jsx, - NavLinks.jsx, - UserLinks.jsx, - LeftSidebar.jsx

---
+++ src/components/Navbar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>
+        <span>App Name</span>
+      </div>
+      <div>NavLinks</div>
+      <div>UserLinks</div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/components/Navbar/UserLinks.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const UserLinks = () => {
+  return <div>UserLinks</div>;
+};
+
+export default UserLinks;
